5243fae6cc4f16d3844b7c019ee4c8af9f4fa636
Carlos Emannoel <53618695+CarlosEmannoel16@users.noreply.github.com>
2021-08-30 19:55:07 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nodets-ProjetoCanil
+Projeto feito no módulo do curso de Node B7web
+
+### Pré requisitos globais
+`npm i -g nodemon typescript ts-node`
+
+
+### Instalação
+`npm init`
+
+### Para rodar projeto
+`npm run start-dev`
---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import * as pageController from '../controllers/pagesController';
+import * as searchController from '../controllers/searchControler';
+
+
+
+const router = Router();
+
+router.get('/',pageController.home);
+router.get('/dogs',pageController.dogs);
+router.get('/cats',pageController.cats);
+router.get('/fishes',pageController.fishes); 
+
+router.get('/search', searchController.search);
+
+export default router;
